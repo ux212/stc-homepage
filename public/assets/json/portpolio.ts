063e72461b5788d8212e65d521a9b0af34f41dbe
Dimi Lee <bash.ruv@kakao.com>
2022-06-17 15:13:29 +0900

    add portfolio logo

--- public/assets/json/portpolio.ts
+++ public/assets/json/portpolio.ts
@@ -2,6 +2,7 @@ import blockcloudLogo from "@public/assets/portfolio/blockcloud.png";
 import choromiaLogo from "@public/assets/portfolio/choromia.png";
 import covenantchildLogo from "@public/assets/portfolio/covenantchild.png";
 import hederahashgraphLogo from "@public/assets/portfolio/hederahashgraph.png";
+import itsblocLogo from "@public/assets/portfolio/itsbloc.png";
 import klaytnLogo from "@public/assets/portfolio/klaytn.png";
 import metabeatLogo from "@public/assets/portfolio/metabeat.png";
 import powerledgerLogo from "@public/assets/portfolio/powerledger.png";
@@ -11,6 +12,7 @@ import tecoLogo from "@public/assets/portfolio/teco.png";
 export const portfolio = [
   metabeatLogo,
   klaytnLogo,
+  itsblocLogo,
   tecoLogo,
   covenantchildLogo,
   choromiaLogo,
